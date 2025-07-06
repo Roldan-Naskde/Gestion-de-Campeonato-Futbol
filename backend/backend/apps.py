07f836c4789672e86024cf14bb1abf2b7956f8fc
Roldan-Naskde <roldan.melendez90@gmail.com>
2025-07-06 18:05:51 -0500

    Updates

--- backend/backend/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class FulbitoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fulbito'
-
-    def ready(self):
-        import backend.signals  # Importa las señales
