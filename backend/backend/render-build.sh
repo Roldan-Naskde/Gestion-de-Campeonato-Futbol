38e38c98eb219f9d1554a5d809482b6d304a40ec
Roldan-Naskde <roldan.melendez90@gmail.com>
2025-07-06 20:29:08 -0500

    arreglo de errores

--- backend/backend/render-build.sh
+++ backend/backend/render-build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # Instalar dependencias
-pip install -r backend/requirements.txt
+pip install -r requirements.txt
 
 # Aplicar migraciones
-python backend/manage.py migrate
+python manage.py migrate
 
 # (Opcional) Crear superusuario automáticamente si deseas
-# python backend/manage.py createsuperuser --noinput (solo si has predefinido un admin)
+# python manage.py createsuperuser --noinput (solo si has predefinido un admin)
 
 echo "Migraciones completadas ✅"
