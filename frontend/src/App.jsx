72e68dc07ab2f9da1e29176fc5af686d1fba4b8e
Roldan-Naskde <roldan.melendez90@gmail.com>
2025-07-06 19:29:52 -0500

    arreglo de errores

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import {Routes, Route } from 'react-router-dom';
 import React from 'react';
+import {Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import PrivateRoute from './components/PrivateRoute';
