26fd2daec25eed0b7634725a667c29439165999e
Roldan-Naskde <roldan.melendez90@gmail.com>
2025-07-11 09:29:17 -0500

    Fix rutas Vite para despliegue en Vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Gestion-de-Campeonato-Futbol/',  // Esta línea es CLAVE
+  base: '/',
 });
