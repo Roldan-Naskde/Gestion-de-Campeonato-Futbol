ea6cd1521620b3757a023ceef8844af3c4338ec3
Roldan-Naskde <roldan.melendez90@gmail.com>
2025-07-11 22:36:41 -0500

    migraciones

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from fulbito.views import generar_tabla_posiciones
 from django.urls import path, include
+from django.core.management import call_command
+from django.http import HttpResponse
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+
+# 🚨 Ruta temporal para migrar la base de datos
+def run_migrations(request):
+    call_command('migrate')
+    return HttpResponse("✅ Migraciones ejecutadas correctamente.")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,4 +20,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('generar-tabla-posiciones/<int:tournament_id>/', generar_tabla_posiciones, name='generar-tabla-posiciones'),
+    path('migrate/', run_migrations),  # Ruta temporal para correr migraciones
 ]
